7044940d77161cd0c2bccd309c81fa64fed79ada
ElMino94 <trestoux@gaming.tech>
2024-12-13 10:57:27 +0100

    test

--- Space Blaster X/caca.cpp
+++ Space Blaster X/caca.cpp
@@ -5,7 +5,7 @@ using namespace sf;
 
 int main() {
     Player player(400.f, 300.f);
-    sf::Clock clock;
+    Clock clock;
     Texture ifmTexture;
     Texture bpTexture;
     Texture vb1Texture;
@@ -20,7 +20,7 @@ int main() {
     Texture i3Texutre;
 
     ifmTexture.loadFromFile("assetocorsa\\image-fond-menu.jpg");
-    bpTexture.loadFromFile("assetocorsa\\bouton-de-jeu.png");
+    //bpTexture.loadFromFile("assetocorsa\\bouton-de-jeu.png");
     vb1Texture.loadFromFile("assetocorsa\\image-fond-menu.jpg");
     vb2Texture.loadFromFile("assetocorsa\\image-fond-menu.jpg");
     vb3Texture.loadFromFile("assetocorsa\\image-fond-menu.jpg");
@@ -38,6 +38,27 @@ int main() {
     Sprite ifmSprite(ifmTexture);
     ifmSprite.setPosition(0, 0);
 
+    Font font;
+    if (!font.loadFromFile("assetocorsa\\Neon Energy x.ttf")) {
+        cerr << "Erreur : Impossible de charger la police !" << endl;
+        return -1;
+    }
+    Text text;
+    text.setFont(font);               
+    text.setString("Space Blaster ");
+    text.setCharacterSize(50);        
+    text.setFillColor(Color::White); 
+    text.setStyle(Text::Bold);    
+    text.setPosition(1000, 0);   
+
+    Text text2;
+    text2.setFont(font);
+    text2.setString("X");
+    text2.setCharacterSize(50);
+    text2.setFillColor(Color::White);
+    text2.setStyle(Text::Bold);
+    text2.setPosition(200, 540);
+
     Sprite bpSprite(bpTexture);
     bpSprite.setPosition(1000, 500);
 
@@ -46,15 +67,19 @@ int main() {
 
 
     while (window.isOpen()) {
-        sf::Time deltaTime = clock.restart();
+        Time deltaTime = clock.restart();
         Event event;
         while (window.pollEvent(event)) {
             if (event.type == Event::Closed)
                 window.close(); 
         }
         player.update(deltaTime.asSeconds());
+
+        window.clear();
         window.draw(ifmSprite);
-        window.draw(bpSprite);
+        //window.draw(bpSprite);
+        window.draw(text);
+        window.draw(text2);
         window.draw(player.ship);
         window.display();
     }
