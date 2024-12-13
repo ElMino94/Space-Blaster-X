d1d8c8ac25a8918a0e9776044d700999683f084e
TeoXpok <goncalves.teo04@gmail.com>
2024-12-13 10:58:34 +0100

    class mob

---
+++ Space Blaster X/mob.h
@@ -0,0 +1,35 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+
+class Mob
+{
+protected:
+
+    int pv;
+    bool invincible;
+    sf::Vector2f position;
+    sf::Vector2f velocity;
+    float acceleration;
+    float friction;
+
+public:
+    Mob(float x, float y, int hp);
+    ~Mob() = default;
+
+    bool isalive();
+    void move(int dx, int dy);
+    bool takedmg(int degat);
+
+    void attack();
+    void update(float deltaTime);
+
+    sf::CircleShape ship;
+
+};
+
+
+
+#endif
--- Space Blaster X/player.cpp
+++ Space Blaster X/player.cpp
@@ -19,7 +19,6 @@ bool Player::takedmg(int degat) {}
 
 void Player::attack() {}
 void Player::update(float deltaTime) {
-    // Applique les entrées utilisateur pour la direction
     sf::Vector2f direction(0.f, 0.f);
 
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
@@ -31,19 +30,13 @@ void Player::update(float deltaTime) {
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
         direction.x += 1.f;
 
-    // Normaliser la direction si nécessaire pour éviter que la vitesse ne soit trop rapide en diagonale
     if (direction.x != 0.f || direction.y != 0.f) {
         float length = sqrt(direction.x * direction.x + direction.y * direction.y);
-        direction /= length; // Normalisation
+        direction /= length;
     }
 
-    // Accélérer dans la direction donnée
     velocity += direction * acceleration * deltaTime;
-
-    // Appliquer la friction pour ralentir le vaisseau
     velocity *= friction;
-
-    // Mettre à jour la position en fonction de la vitesse
     position += velocity * deltaTime;
 
     ship.setPosition(position);
