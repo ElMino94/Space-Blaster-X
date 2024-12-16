fe0e00273aa45f0a9831eb8098e6b2c6a21aeb8e
ElMino94 <trestoux@gaming.tech>
2024-12-16 03:28:16 +0100

    test

--- Space Blaster X/caca.cpp
+++ Space Blaster X/caca.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 #include "player.h"
 using namespace std;
 using namespace sf;
@@ -14,64 +15,47 @@ int main() {
     Player player(400.f, 300.f);
     Clock clock;
     Texture ifmTexture;
-    Texture bpTexture;
-    Texture vb1Texture;
-    Texture vb2Texture;
-    Texture vb3Texture;
-    Texture ve1Texture;
-    Texture ve2Texture;
-    Texture ve3Texture;
-    Texture vpTexture; 
-    Texture i1Texutre;
-    Texture i2Texutre;
-    Texture i3Texutre;
+    Texture ftmTexture;
 
     ifmTexture.loadFromFile("assetocorsa\\image-fond-menu.jpg");
-    //bpTexture.loadFromFile("assetocorsa\\bouton-de-jeu.png");
-    vb1Texture.loadFromFile("assetocorsa\\image-fond-menu.jpg");
-    vb2Texture.loadFromFile("assetocorsa\\image-fond-menu.jpg");
-    vb3Texture.loadFromFile("assetocorsa\\image-fond-menu.jpg");
-    ifmTexture.loadFromFile("assetocorsa\\image-fond-menu.jpg");
-    ifmTexture.loadFromFile("assetocorsa\\image-fond-menu.jpg");
-    ifmTexture.loadFromFile("assetocorsa\\image-fond-menu.jpg");
-    ifmTexture.loadFromFile("assetocorsa\\image-fond-menu.jpg");
-    ifmTexture.loadFromFile("assetocorsa\\image-fond-menu.jpg");
-    ifmTexture.loadFromFile("assetocorsa\\image-fond-menu.jpg");
-    ifmTexture.loadFromFile("assetocorsa\\image-fond-menu.jpg");
+    ftmTexture.loadFromFile("assetocorsa\\button_square_header_small_rectangle_screws.png");
+    
 
-    RenderWindow window(VideoMode(1920, 1080), "Space Blaster X", Style::None);
+    RenderWindow window(VideoMode(1920, 1080), "Space Blaster X");//, Style::None
     window.setFramerateLimit(9999999999999999999);
 
     Sprite ifmSprite(ifmTexture);
     ifmSprite.setPosition(0, 0);
+
+    Sprite ftmSprite(ftmTexture);
+    ftmSprite.setPosition(790, 150);
+    ftmSprite.setScale(1.3f, 1, 3f);
 
     Font font;
     if (!font.loadFromFile("assetocorsa\\Neon Energy x.ttf")) {
         cerr << "Erreur : Impossible de charger la police !" << endl;
         return -1;
     }
+
+    //
+    //
+
     Text text;
+
     text.setFont(font);               
     text.setString("Space Blaster ");
     text.setCharacterSize(50);        
     text.setFillColor(Color::White); 
     text.setStyle(Text::Bold);    
-    text.setPosition(1000, 0);   
+    text.setPosition(800, 153);   
 
     Text text2;
     text2.setFont(font);
     text2.setString("X");
-    text2.setCharacterSize(50);
+    text2.setCharacterSize(75);
     text2.setFillColor(Color::White);
     text2.setStyle(Text::Bold);
-    text2.setPosition(200, 540);
-
-    Sprite bpSprite(bpTexture);
-    bpSprite.setPosition(1000, 500);
-
-
-
-
+    text2.setPosition(960, 200);
 
     while (window.isOpen()) {
         Time deltaTime = clock.restart();
@@ -86,7 +70,7 @@ int main() {
 
         
         window.draw(ifmSprite);
-        //window.draw(bpSprite);
+        window.draw(ftmSprite);
         window.draw(text);
         window.draw(text2);
         window.draw(player.ship);
