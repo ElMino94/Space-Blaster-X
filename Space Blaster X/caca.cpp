02245ab83b48d816e95d62d9ea9c996ee77ee712
ElMino94 <trestoux@gaming.tech>
2024-12-16 07:02:09 +0100

    test

--- Space Blaster X/caca.cpp
+++ Space Blaster X/caca.cpp
@@ -4,6 +4,9 @@
 using namespace std;
 using namespace sf;
 
+//""Sprite.setScale(1.3f, 1.3f); augmente la taille du sprite
+//text.setStyle(sf::Text::Underlined); // Applique un soulignement
+
 void initialisation() {
 
 
@@ -15,21 +18,26 @@ int main() {
     Player player(400.f, 300.f);
     Clock clock;
     Texture ifmTexture;
-    Texture ftmTexture;
+    Texture bpTexture;
+    Texture setTexture;
+    Texture exTexture;
 
     ifmTexture.loadFromFile("assetocorsa\\image-fond-menu.jpg");
-    ftmTexture.loadFromFile("assetocorsa\\button_square_header_small_rectangle_screws.png");
+    bpTexture.loadFromFile("assetocorsa\\bp.png");
+    setTexture.loadFromFile("assetocorsa\\set.png");
+    exTexture.loadFromFile("assetocorsa\\ex.png");
     
-
     RenderWindow window(VideoMode(1920, 1080), "Space Blaster X");//, Style::None
     window.setFramerateLimit(9999999999999999999);
 
     Sprite ifmSprite(ifmTexture);
     ifmSprite.setPosition(0, 0);
-
-    Sprite ftmSprite(ftmTexture);
-    ftmSprite.setPosition(790, 150);
-    ftmSprite.setScale(1.3f, 1, 3f);
+    Sprite bpSprite(bpTexture);
+    bpSprite.setPosition(50, 200);
+    Sprite setSprite(setTexture);
+    setSprite.setPosition();
+    Sprite exSprite(exTexture);
+    exSprite.setPosition();
 
     Font font;
     if (!font.loadFromFile("assetocorsa\\Neon Energy x.ttf")) {
@@ -37,46 +45,44 @@ int main() {
         return -1;
     }
 
-    //
-    //
-
     Text text;
 
     text.setFont(font);               
-    text.setString("Space Blaster ");
-    text.setCharacterSize(50);        
-    text.setFillColor(Color::White); 
-    text.setStyle(Text::Bold);    
-    text.setPosition(800, 153);   
-
-    Text text2;
-    text2.setFont(font);
-    text2.setString("X");
-    text2.setCharacterSize(75);
-    text2.setFillColor(Color::White);
-    text2.setStyle(Text::Bold);
-    text2.setPosition(960, 200);
+    text.setString("Space Blaster X");
+    text.setCharacterSize(75);        
+    text.setFillColor(Color(16, 39, 117));
+    text.setStyle((Text::Bold | Text::Underlined));
+    text.setPosition(50, 50);
 
     while (window.isOpen()) {
         Time deltaTime = clock.restart();
         Event event;
         while (window.pollEvent(event)) {
             if (event.type == Event::Closed)
-                window.close(); 
-            if (event.key.code == sf::Keyboard::Escape)
                 window.close();
         }
+
+        if (Mouse::isButtonPressed(Mouse::Left)) {
+            Vector2i mousePos = Mouse::getPosition(window);
+
+            if (bpSprite.getGlobalBounds().contains(mousePos.x, mousePos.y)) {
+
+            }
         player.update(deltaTime.asSeconds());
 
         
         window.draw(ifmSprite);
-        window.draw(ftmSprite);
         window.draw(text);
-        window.draw(text2);
-        window.draw(player.ship);
+        //window.draw(player.ship);
+        window.draw(bpSprite);
         window.display();
         window.clear();
     }
 
     return 0;
 }
+
+//Comment faire le changement de menu??
+//
+//Idee de teva: 1........Si play --> open menu diff --> lancement du jeu en fonction de la difficulte choisi!!
+//              2........Faire un fichier pour chaque etape pour les ramener dans le caca.cpp 
